b9290a0d815c20e338d7e027f8a50dc592c9e135
Karim Roshdy <karimroshdy911@gmail.com>
2025-04-10 23:40:39 +0200

    improve test cases

--- Personal-Finance-Tracker/src/main/kotlin/test/TransactionRemoveTest.kt
+++ Personal-Finance-Tracker/src/main/kotlin/test/TransactionRemoveTest.kt
@@ -1,33 +1,61 @@
 package org.example.test
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import models.Category
+import models.Transaction
+import org.example.common.check
+import org.example.datasource.FakeTransactionDataSourceImpl
 
 fun main(){
+    val dataSource = FakeTransactionDataSourceImpl()
+
+    val validWithdrawalTransaction = Transaction(
+        name = "Withdrawal Transaction",
+        isDeposit = false,
+        amount = 150.0,
+        category = Category.RENT,
+        creationDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+        editDate = emptyList()
+    )
+
+    val validDepositTransaction = Transaction(
+        name = "Deposit Transaction",
+        isDeposit = true,
+        amount = 150.0,
+        category = Category.RENT,
+        creationDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+        editDate = emptyList()
+    )
+    val validDepositTransaction2 = Transaction(
+        name = "Deposit Transaction",
+        isDeposit = true,
+        amount = 150.0,
+        category = Category.RENT,
+        creationDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date,
+        editDate = emptyList()
+    )
 
     check(
         name = "When removing from empty list, then should return false",
-        result=false,
+        actual=dataSource.removeTransaction(validWithdrawalTransaction),
         expected =  false
     )
     check(
         name = "When removing existing transaction, then should return true",
-        result = true,
+        actual = dataSource.removeTransaction(validDepositTransaction),
         expected = true
     )
     check(
         name = "When removing same data but different ID, then should return false",
-        result =  false ,
+        actual =  dataSource.removeTransaction(validDepositTransaction) ,
         expected =  false
     )
     check(
         name = "When removing one of multiple transactions, then should return true",
-        result= true,
+        actual= dataSource.removeTransaction(validDepositTransaction2),
         expected = true
     )
+
 }
 
-fun <T>check(name: String, result: T, expected: T) {
-    if (result == expected) {
-        println("Successful: $name")
-    } else {
-        println("X-Failed: $name | Expected: $expected, Got: $result")
-    }
-}
