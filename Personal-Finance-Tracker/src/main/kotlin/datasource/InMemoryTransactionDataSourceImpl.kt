9063f60ba415d21c7c2d5e662efb1345988f8934
Mahmoud Tarek <dev.mahmoudtarek@gmail.com>
2025-04-10 14:24:27 +0200

    implement removeTransaction function

--- Personal-Finance-Tracker/src/main/kotlin/datasource/InMemoryTransactionDataSourceImpl.kt
+++ Personal-Finance-Tracker/src/main/kotlin/datasource/InMemoryTransactionDataSourceImpl.kt
@@ -4,14 +4,15 @@ import datasource.TransactionDataSource
 import models.Transaction
 import java.time.LocalDateTime
 
-class InMemoryTransactionDataSourceImpl:TransactionDataSource {
+class InMemoryTransactionDataSourceImpl : TransactionDataSource {
+    val transactionList = mutableListOf<Transaction>()
+
     override fun createTransaction(transaction: Transaction) {
         TODO("Not yet implemented")
     }
 
-    override fun removeTransaction(transaction: Transaction) {
-        TODO("Not yet implemented")
-    }
+    override fun removeTransaction(transaction: Transaction) =
+        transactionList.removeIf { it.id == transaction.id }
 
     override fun updateTransaction(transaction: Transaction) {
         TODO("Not yet implemented")
