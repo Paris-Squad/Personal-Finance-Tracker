b3aa9ed6cfc0fc55206548ac4548dc69e4f5d5a5
Ahmed Atef <atef.ahmed1341@gmail.com>
2025-04-12 01:47:23 +0200

    update test cases for get transaction

--- Personal-Finance-Tracker/src/main/kotlin/test/TransactionRetrieveTest.kt
+++ Personal-Finance-Tracker/src/main/kotlin/test/TransactionRetrieveTest.kt
@@ -3,22 +3,17 @@ package org.example.test
 import models.Transaction
 import org.example.datasource.InMemoryTransactionDataSourceImpl
 
-fun checkTransaction(name: String, actual: List<Transaction>, expected: List<Transaction>) = actual == expected
+fun checkTransaction(name: String, actual: List<Transaction>, expected: List<Transaction>) {
+    if (actual == expected)
+        println("Success!")
+    else
+        println("Failed!")
+}
 
 fun checkGetTransactionTestCases() {
-    checkTransaction( //"print anything during logic time"
+    checkTransaction(
         name = "When the list returned is empty, return true",
         actual = InMemoryTransactionDataSourceImpl().getAllTransaction(),
         expected = emptyList()
-    )
-    checkTransaction(
-        name = "When returned list of type \"Any\", return false",
-        actual = InMemoryTransactionDataSourceImpl().getAllTransaction(),
-        expected = emptyList() //Expected to be "null" not an empty list.
-    )
-    checkTransaction(
-        name = "When the returned list is of type \"Transaction\", return true",
-        actual = InMemoryTransactionDataSourceImpl().getAllTransaction(),
-        expected = listOf<Transaction>()
     )
 }
